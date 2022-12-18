b9bc7a418c4186ede2769d5ab3e097886d44c3d7
VladosikJoskii <vlad2008maxytenko@gmail.com>
2022-12-18 13:50:12 +0200

    adds test AT00003.py

--- code/tests/test_AT00003.py
+++ code/tests/test_AT00003.py
@@ -5,16 +5,19 @@ import sys
 import importlib.util
 
 '''
-Date: 
-Tester Name: 
+Date: 17.12.22
+Tester Name: Vladislav Maksiutenko
 Ticket: АТ00003
-Short Description: 
+Short Description: Check the name of the site on the main page
 Component: PT0000
 '''
 def test_AT00003():
     # passing the file name and path as argument
     spec = importlib.util.spec_from_file_location(
-  "page_class", "ВАШ ПУТЬ ДО page_class.py")
+  "page_class", "C:/Users/Андрей/Desktop/Код/Python-online-QA-Automation/code/framework/page_class.py")
     page_cls = importlib.util.module_from_spec(spec) 
     spec.loader.exec_module(page_cls)
-    # TODO дописать логику
+    website = page_cls.PageObject()
+    website.assert_text_in_block('пятый элемент-днепр',
+                                 "//div[@class='c-name']/h1",
+                                 'xpath')
